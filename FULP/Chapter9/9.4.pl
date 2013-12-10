e9969ab83808dda9937c88135c9742255a13751d
DennisBlock <dennis.block@gmx.de>
2013-12-10 10:01:04 +0100

    asd

--- FULP/Chapter9/9.4.pl
+++ FULP/Chapter9/9.4.pl
@@ -1,1 +1,17 @@
-%groundterm(Term):- 
+groundterm(T) :- nonvar(T), atomic(T), !.
+
+groundterm(T) :- nonvar(T),
+functor(T,_,A),
+groundterm(A,T).
+
+groundterm(N,T) :- N > 0, arg(N, T, Arg), groundterm(Arg), N1 is N - 1, groundterm(N1, T).
+groundterm(0,_). 
+
+
+%%% args !?!?
+args(T,Args) :- nonvar(T),
+functor(T,_,A),
+acc_args(A,T,[],Args).
+
+acc_args(N,T,Acc,Args):- N > 0, arg(N,T,Arg), N1 is N - 1, NewAcc = [Arg|Acc], acc_args(N1, T, NewAcc, Args).
+acc_args(0,-,Args,Args).
---
+++ RandomWalk/src/de/fhswf/randomwalk/RandomWalk.java
@@ -0,0 +1,9 @@
+package de.fhswf.randomwalk;
+
+public class RandomWalk {
+
+	public static void main(String[] args) {
+
+	}
+
+}
